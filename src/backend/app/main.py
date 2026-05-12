1d3128e094218381f100d0b90ab5fb64494bd3e5
teresiawairimu <teresiawairimu06.gmail.com>
2026-05-11 23:17:40 -0400

    create db, schema, model

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,2 +1,13 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Job Application Tracker API")
+
+@app.get("/health")
+def health_check():
+  return {"status": "ok"}
+
 
