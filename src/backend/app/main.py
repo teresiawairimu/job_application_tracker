9418d329240d8413cb71db9c90a8366e6cb58195
teresiawairimu <teresiawairimu06.gmail.com>
2026-05-17 21:51:33 -0400

    setting up alembic migration

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -3,7 +3,7 @@ from app.database import Base, engine
 from app.models.user import User
 from app.routers import auth, application, company, note, user
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Job Application Tracker API")
 
