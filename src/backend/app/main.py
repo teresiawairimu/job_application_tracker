4bca7a7aebfe70cb1987252bd8c4e375bef01614
teresiawairimu <teresiawairimu06.gmail.com>
2026-06-04 14:25:45 -0400

    added CORS

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,11 +1,26 @@
+import os
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models.user import User
 from app.routers import auth, application, company, note, user
+from fastapi.middleware.cors import CORSMiddleware
 
 #Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Job Application Tracker API")
+
+cors_origins = os.getenv(
+  "CORS_ORIGINS",
+  "http://localhost:3000, http://localhost:5173"
+).split(",")
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=[origin.strip() for origin in cors_origins],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=[""],
+)
 
 app.include_router(auth.router)
 app.include_router(application.router)
